76750195282aeaf32a41fa649e1e48eb102fca00
HARRY DENLEY <HARRY.DENLEY@OUTLOOK.COM>
2021-04-03 18:43:45 +0100

    Added tweet functionality

--- src/index.php
+++ src/index.php
@@ -9,6 +9,18 @@ if (file_exists(__DIR__ . '/../.env')) {
     $dotenv->load();
 }
 
+$arrTwitterSettings = array(
+    'oauth_access_token' => $_ENV["TWITTER_OAUTH_ACCESS_TOKEN"],
+    'oauth_access_token_secret' => $_ENV["TWITTER_OAUTH_ACCESS_TOKEN_SECRET"],
+    'consumer_key' => $_ENV["TWITTER_CONSUMER_KEY"],
+    'consumer_secret' => $_ENV["TWITTER_CONSUMER_SECRET"]
+);
+
 $objPhraser = new Phraser;
 
-var_dump($objPhraser->generate());
+$objTwitter = new TwitterAPIExchange($arrTwitterSettings);
+$strTweetResponse = $objTwitter->buildOauth('https://api.twitter.com/1.1/statuses/update.json', "POST")
+    ->setPostfields([
+        "status" => $objPhraser->generate()
+    ])
+    ->performRequest();
